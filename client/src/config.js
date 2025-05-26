dad68d8cbf14db0e4f8ccccf0a88582a1eba33fa
Akshay Gohel <108362602+Akshay3237@users.noreply.github.com>
2025-05-26 07:11:06 +0530

    Update config.js
    updated for change url from local to online deploying version.

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,6 @@
-const API_BASE_URL = 'http://localhost:5000'; // Change only here for other environments
+//const API_BASE_URL = 'http://localhost:5000'; // Change only here for other environments
+
+//export default API_BASE_URL;
+const API_BASE_URL = 'https://agentappoint.onrender.com'; // Change only here for other environments
 
 export default API_BASE_URL;
